8b0d06c837e3508309c8f1d4c2b63f35187663aa
heedohee <ydohee0907@gmail.com>
2025-03-05 19:24:04 +0900

    Save

---
+++ NetworkFramework/NetworkFramework/TestClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using Do.Net;
+using Packets;
+using System.Net;
+
+namespace TestClient
+{
+    internal class Program
+    {
+        public static Connector connector;
+        public static ServerSession serverSession;
+
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("172.31.1.191");
+            IPEndPoint endPoint = new(ipAddress, 8081);
+
+            serverSession = new();
+
+            connector = new(endPoint, serverSession);
+            connector.StartConnect(endPoint);
+
+            while (true) { }
+        }
+
+        public static async void Chatting()
+        {
+            while (true)
+            {
+                if (serverSession.Active == 0)
+                    break;
+
+                string message = Console.ReadLine();
+                C_ChatPacket chatPacket = new();
+                chatPacket.message = message;
+
+                serverSession.Send(chatPacket.Serialize());
+            }
+        }
+    }
+}
---
+++ NetworkFramework/NetworkFramework/TestServer/ClientSession.cs
@@ -0,0 +1,34 @@
+﻿using Do.Net;
+using System.Net;
+
+namespace TestServer
+{
+    public class ClientSession : Session
+    {
+        public ChatRoom Room;
+        public EndPoint EndPoint;
+
+        public override void OnConnected(EndPoint endPoint)
+        {
+            EndPoint = endPoint;
+            Program.Room.Push(() => Program.Room.Enter(this, endPoint));
+            Console.WriteLine($"[Session] Client Connected : {endPoint}");
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"[Session] Client Disconnected : {endPoint}");
+        }
+
+        public override void OnPacketReceived(ArraySegment<byte> buffer)
+        {
+            Console.WriteLine($"[Session] {buffer.Count} of Data Received");
+            PacketManager.Instance.HandlePacket(this, buffer);
+        }
+
+        public override void OnSent(int length)
+        {
+            Console.WriteLine($"[Session] {length} of Data Sent");
+        }
+    }
+}
